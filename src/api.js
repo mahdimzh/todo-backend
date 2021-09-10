4d9c801c163aa1968615d578be0c7547d8a1c720
Mahdi Mohammadizadeh <ma.mohammadizadeh@gmail.com>
2021-09-10 17:18:47 +0430

    first implementation of sorting list

--- src/api.js
+++ src/api.js
@@ -28,7 +28,7 @@ app.use(express.urlencoded({ extended: false }));
 
 app.get('/', async (req, res) => {
   const todos = database.client.db('todos').collection('todos');
-  const response = await todos.find({}).toArray();
+  const response = await todos.find({}).sort( { seq: 1 } ).toArray();
   res.status(200);
   res.json(response);
 });
@@ -41,8 +41,10 @@ app.post('/', async (req, res) => {
     res.json({ message: "invalid 'text' expected string" });
     return;
   }
+  const maxSeq = await database.client.db('todos').collection('todos').find().sort({"seq" : -1}).limit(1).toArray();
 
-  const todo = { id: generateId(), text, completed: false };
+  const todo = { id: generateId(), text, completed: false, seq: maxSeq.length > 0 ? maxSeq[0].seq + 1 : 1 };
+
   await database.client.db('todos').collection('todos').insertOne(todo);
   res.status(201);
   res.json(todo);
@@ -62,6 +64,30 @@ app.put('/:id', async (req, res) => {
     { $set: { completed } });
   res.status(200);
   res.end();
+});
+
+app.post('/reorder', async (req, res) => {
+  const { sourceId, destinationId } = req.body;
+
+  const query = {
+    $or: [
+      { id: sourceId },
+      { id: destinationId },
+    ],
+  };
+
+  const reorderList = await database.client.db('todos').collection('todos').find(query).toArray();
+  if(reorderList.length === 2) {
+    await database.client.db('todos').collection('todos').updateOne({ id: reorderList[0].id },
+    { $set: { seq: reorderList[1].seq } });
+    await database.client.db('todos').collection('todos').updateOne({ id: reorderList[1].id },
+    { $set: { seq: reorderList[0].seq } });
+  }
+
+  res.status(200);
+  res.json(reorderList);
+
+  //res.end();
 });
 
 app.delete('/:id', async (req, res) => {
