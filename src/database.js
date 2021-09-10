f2fafbb33e40387fe6026f8e7ed229943fb86d95
Mahdi Mohammadizadeh <ma.mohammadizadeh@gmail.com>
2021-09-10 17:18:23 +0430

    add config for database

--- src/database.js
+++ src/database.js
@@ -1,7 +1,13 @@
 const { MongoClient } = require('mongodb');
+const config = require('config');
 
 const database = module.exports;
 
+const DB_HOST = config.get('dbConfig.host');
+const MONGOPORT = config.get('dbConfig.port');
+const MONGO_COLLECTION = config.get('dbConfig.dbCollection');
+const MONGO_URI = `mongodb://${DB_HOST}:${MONGOPORT}/${MONGO_COLLECTION}`;
+
 database.connect = async function connect() {
-  database.client = await MongoClient.connect('mongodb://root:1234@database/', { useUnifiedTopology: true });
+  database.client = await MongoClient.connect(MONGO_URI, { useUnifiedTopology: true });
 };
